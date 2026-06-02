6dcb9e573c1000289112a701c8e0375fad846450
David <en.mail.til.david@gmail.com>
2026-06-02 10:23:04 +0200

    Roll dice

--- Game_logic.cs
+++ Game_logic.cs
@@ -4,7 +4,30 @@ namespace LiarsDice
     {
     public static void Start()
         {
-             Console.WriteLine("test");
+            int[] dice = new int[5];
+            RollDice(dice);
+            PrintDice(dice);
+            
+        }
+                public static void PrintDice(int[] dice)
+        {
+            for (int i = 0; i < dice.Length; i++)
+            {
+                Console.WriteLine($"Die {i + 1}: {dice[i]}");
+            }
+        }
+            public static int[] RollDice(int[] dice)
+        {
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i] = RandomInt(1, 7);
+            }
+            return dice;
+        }
+            public static int RandomInt(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
         }
     }
 }
