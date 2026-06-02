0401d25f109e58ddd68f89e96cec75bf0642daf6
David <en.mail.til.david@gmail.com>
2026-06-02 10:47:55 +0200

    boring, symatics and making Console less chaotic

--- Game_logic.cs
+++ Game_logic.cs
@@ -6,25 +6,29 @@ namespace LiarsDice
         {
             int[] dice = new int[5];
             RollDice(dice);
-            PrintDice(dice);
 
             int[] currentBid = new int[2];
             while (true)
             {
+                Console.Clear();
+                PrintDice(dice);
                 currentBid = GetPlayerBid(); 
             }
         }
         public static int[] GetPlayerBid()
         {
+            try{
             Console.WriteLine("Enter your bid (quantity and face value): ");
             string input = Console.ReadLine();
             string[] parts = input.Split(' ');
-            try{
+            
             if (int.Parse(parts[1]) >= 7)
                 {
                     throw new Exception();
                 }
+
             return new int[]{int.Parse(parts[0]), int.Parse(parts[1])};
+
             }
             catch
             {
